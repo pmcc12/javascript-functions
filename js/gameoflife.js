7f2066e1e89f309c1449efef5b8f66c54270a039
Miguel Carvalho <pmccarvalho12@gmail.com>
2020-08-30 15:40:41 +0100

    30.08 2.2

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -6,7 +6,14 @@ function seed(a,b,c) {
   return array_seed;
 }
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  if(x===j && y===k){
+    return true;
+  } else{
+    return false;
+  }
+
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {}
